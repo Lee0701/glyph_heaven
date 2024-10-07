27ddf4086fd4233f529a1de2b411a4bf7c150341
Lee0701 <hgl1002@naver.com>
2024-10-08 02:22:10 +0900

    Add bulma css

--- theme/src/index.ts
+++ theme/src/index.ts
@@ -2,5 +2,20 @@
 import style from './style.scss'
 
 document.addEventListener('DOMContentLoaded', () => {
+    setupNavbar()
     style.init()
 })
+
+function setupNavbar() {
+    const burger = document.querySelector('.navbar-burger')
+    if(burger instanceof HTMLElement) {
+        burger?.addEventListener('click', () => {
+            const targetId = burger.dataset.target
+            if(!targetId) return
+            const target = document.getElementById(targetId)
+            if(!target) return
+            burger.classList.toggle('is-active')
+            target.classList.toggle('is-active')
+        })
+    }
+}
