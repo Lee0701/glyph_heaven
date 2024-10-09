83ebfd69919e85f69b502fdf3e01308b35cf53b9
Lee0701 <hgl1002@naver.com>
2024-10-09 19:33:49 +0900

    Fix kage tag styles

--- glyphs/kage_gen.py
+++ glyphs/kage_gen.py
@@ -13,7 +13,7 @@ for name, data in kage_data.items():
     kage.components.push(name, data)
 
 def render(data):
-    drawing = Drawing(size=(20, 20))
+    drawing = Drawing(size=(16, 16))
     drawing = kage.make_glyph_with_data(drawing, data)
     if drawing is not None:
         drawing.viewbox(0, 0, 200, 200)
